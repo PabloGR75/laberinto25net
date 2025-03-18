bde7d5569e1ec742a415ed9dd7a26a6389e95227
PabloGR75 <pgarciarubio75@gmail.com>
2025-03-18 19:49:07 +0100

    Añadir más ficheros de configuración

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HolaMundoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+
+            // Crear una instancia de la clase Persona
+            Persona persona = new Persona("Juan", 30);
+            Console.WriteLine($"Nombre: {persona.Nombre}, Edad: {persona.Edad}");
+        }
+    }
+
+    // Definición de la clase Persona con atributos (propiedades)
+    class Persona
+    {
+        // Propiedades (atributos) de la clase
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        // Constructor para inicializar la clase
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+    }
+}
